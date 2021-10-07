de8bd3b13e06194860c17c52846c0c682f278bca
Nadiia-Myronenko <nadegda.burina@gmail.com>
2021-10-07 15:02:24 +0300

    Добавлен функционал изменения темы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import './sass/main.scss';
+import './styles.css';
+
+// Добавление функционала изменения темы при нажатии на чекбокс #theme-switch-toggle в тулбаре.
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    body: document.querySelector('body'),
+    themeSwitch: document.querySelector('#theme-switch-toggle')
+}
+
+themeDefaultSetting();
+
+refs.themeSwitch.addEventListener('change', onThemeSwitchChange);
+
+function onThemeSwitchChange() {
+    refs.body.classList.toggle(Theme.DARK);
+
+    if (refs.body.classList.contains(Theme.DARK)) {
+        localStorage.setItem('theme', Theme.DARK)
+    } else { localStorage.setItem('theme', Theme.LIGHT) };
+}
+
+function themeDefaultSetting() {
+    if (!localStorage.length) {
+        localStorage.setItem('theme', Theme.LIGHT)
+    };
+    refs.body.classList.add(localStorage.getItem('theme'));
+    
+    if (refs.body.classList.contains(Theme.DARK)) {
+        refs.themeSwitch.checked = true;
+    }
+}
